71f4cc75ed77d1c9d4d0974e7eb4bd3a9bf07262
Yash_Thakur <104083112+yashthakur2211@users.noreply.github.com>
2023-11-06 22:25:30 +0530

    Update SL-1 Assignment 1.py

--- SL-1 Assignment 1.py
+++ SL-1 Assignment 1.py
@@ -1,30 +1,49 @@
+print("DFS is :")
+graph={'a':['b','c'],
+       'b':['d','e'],
+       'c':['f'],
+       'd':[],
+       'e':['f'],
+       'f':[]}   
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        
+        for neighbor in graph[node]:
+            dfs(visited, graph, neighbor)
+dfs(visited, graph, 'a')
+
+print("\n")
+print("BFS is :")
+
 graph = {
-0: [1, 2],
-1: [0, 3, 4],
-2: [0,3],
-3: [1,2,4],
-4: [1,3]
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
 }
-x=int(input("How many Nodes are there in the graph :"))
-visited=[0]*x #visited=[0,0,0,0,0]
-def dfs(graph, s):#actual GRAPH ani STARTING vertex eg.0
-visited[s]=1 #visited[0]=1 ==[1,0,0,0,0]
-print(s)
-for c in graph[s]:
-if visited[c]==0:
-dfs(graph,c)
-def bfs(graph,s):
-Queue=[s] #queue=[0]
-visitedd=[s] #visitedd=[0]
-while Queue: #until Queue contains some values
-cur=Queue.pop(0)
-print(cur)
-for c in graph[cur]:
-if c not in visitedd:
-Queue.append(c)
-visitedd.append(c)
-p=int(input("Choose DFS OR BFS :\n1).DFS\n2).BFS\n---->"))
-if p==1:
-dfs(graph,0)                  
-if p==2:
-bfs(graph,0)
+
+visited = []
+queue = []
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+bfs(visited, graph, 'A')
+
+print("\n")
